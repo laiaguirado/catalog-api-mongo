2d48f89825275074c87b9a14dc9063124187acdf
Laia Guirado Campmany <laiaguirado1999@gmail.com>
2022-05-30 21:05:59 +0200

    Adding to endpoints

--- server/src/resources/catalog/catalog.controllers.js
+++ server/src/resources/catalog/catalog.controllers.js
@@ -10,5 +10,58 @@ const findMany = async (req,res)=>{
     }
 }
 
+const createOne = async (req,res)=>{
+    try{
+        const newCatalog = req.body;
+        const doc = await Catalog.create(newCatalog);
+        res.status(200).json({results: doc})
+    }catch(e){
+        console.log(e);
+        res.status(500).json({error:'Creation failed'})
+    }
+}
 
-module.exports = { findMany };
+const findOne = async (req, res) =>{
+    try{
+        const {id} = req.params;
+        const doc = await Catalog.find({_id: id});
+        if(!doc){
+            return res.status(404).json({error:'Not found'});
+        }
+        res.status(200).json({results:doc});
+    }catch(e){
+        console.log(e);
+        res.status(500).json({error:'Internal error'});
+    }
+}
+
+const updateOne = async (req,res)=>{
+//"new: true" to return the document after update
+try{
+    const {id} = req.params;
+    const doc = await Catalog.findOneAndUpdate({_id: id},req.body,{new:true}); 
+    if(!doc){
+        return res.status(404).json({error:'Not found'});
+    }
+    res.status(200).json({results:doc});
+}catch(e){
+    console.log(e);
+    res.status(500).json({error:'Cannot update'});
+}
+}
+
+const deleteOne = async (req,res)=>{
+    try{
+        const {id} = req.params;
+        const doc = await Catalog.findOneAndDelete({_id: id}); 
+        if(!doc){
+            return res.status(404).json({error:'Not found'});
+        }
+        res.status(200).json({results:doc});
+    }catch(e){
+        console.log(e);
+        res.status(500).json({error:'Cannot delete'});
+    }
+}
+
+module.exports = { findMany, createOne, findOne, updateOne,deleteOne };
--- server/src/resources/catalog/catalog.router.js
+++ server/src/resources/catalog/catalog.router.js
@@ -4,5 +4,9 @@ const catalogControllers = require("./catalog.controllers");
 const router = Router();
 
 router.get("/", catalogControllers.findMany);
+router.post("/", catalogControllers.createOne);
+router.get("/:id", catalogControllers.findOne);
+router.put("/:id", catalogControllers.updateOne);
+router.delete("/:id", catalogControllers.deleteOne);
 
 module.exports = router;
